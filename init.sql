36d1c5129b2102ff84adc6da162b1b7f4180f17d
Alicecaglisse01 <alice0404.moisan@gmail.com>
2024-03-12 17:19:06 +0100

    toutes les requetes fonctionnent (users)

--- init.sql
+++ init.sql
@@ -5,5 +5,14 @@ CREATE TABLE products (
   price FLOAT
 );
 
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(128) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL
+);
+
 INSERT INTO products (name, about, price) VALUES
   ('My first game', 'This is an awesome game', '60')
+
+
